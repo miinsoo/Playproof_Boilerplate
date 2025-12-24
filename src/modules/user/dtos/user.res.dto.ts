03b79e5ec8b08b17c018c94bbaf19f0dc9e2b8f4
rbdus0715 <rbdus0715@gmail.com>
2025-12-24 22:03:02 +0900

    Chore: dto 기본 설정

--- src/modules/user/dtos/user.res.dto.ts
+++ src/modules/user/dtos/user.res.dto.ts
@@ -1,20 +1,5 @@
-// src/modules/user/dtos/user.res.dto.ts
-export interface UserSignUpResDto {
-  /**
-   * 유저의 고유 ID (DB Primary Key)
-   * @example 1
-   */
-  id: number;
-
-  /**
-   * 유저 이메일
-   * @example "user@example.com"
-   */
-  email: string;
-
-  /**
-   * 유저 이름 
-   * @example "홍길동"
-   */
-  name: string;
+export class UserSignUpResDto {
+  id!: string
+  email!: string;
+  name!: string;
 }
